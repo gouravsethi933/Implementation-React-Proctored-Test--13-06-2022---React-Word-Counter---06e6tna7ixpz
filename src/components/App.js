2b270aca20597e22e9af7387c2b37139dd212adc
Gourav Sethi <61932647+gouravsethi933@users.noreply.github.com>
2022-06-13 20:33:40 +0530

    string

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,55 @@
 import React, { useState } from "react";
 import "../styles/App.css";
 const App = () => {
-  const [text, settext] = useState("");
-  const [size, setsize] = useState(0);
-  const [max, setmax] = useState(50);
-
+  const [content, setContent] = useState(null);
+  const [fontSize, setFontSize] = useState(16);
+  const [limit, setLimit] = useState(50);
+  const getWords = () => {
+    return content == null || content == ""
+      ? 0
+      : content.trim().split(" ").length;
+  };
+  const getChar = () => {
+    return content == null || content == "" ? 0 : content.split("").length;
+  };
   return (
     <div id="main">
-      <textarea
-        maxlength={max}
-        onChange={(e) => settext(e.target.value)}
-      ></textarea>
-      <div className="word-counter">
-        <p>Total number of words written {text.split(" ").length}</p>
-      </div>
-      <div id="char-counter">
-        <p>Total number of charters used {text.length}</p>
+      <div>
+        <input
+          type="range"
+          id="fontSize-input"
+          min="16"
+          max="32"
+          value={fontSize}
+          onChange={(e) => setFontSize(e.target.value)}
+        />
       </div>
       <br />
-      <h3>Word Limit Input</h3>
-      <input
-        type="number"
-        value={max}
-        onChange={(e) => setmax(e.target.value)}
-        id="char-limit-input"
-      />
+      <div>
+        <input
+          type="text"
+          id="char-limit-input"
+          value={limit}
+          onChange={(e) => setLimit(e.target.value)}
+        />
+      </div>
       <br />
-      <h3>Font Size Picker</h3>
-      <input
-        type="range"
-        max="32px"
-        min="16"
-        onChange={(e) => setsize(e.target.value)}
-      />
+      <div>
+        <textarea
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+          style={{ fontSize: fontSize + "px" }}
+          maxLength={limit}
+        >
+          {content}
+        </textarea>
+      </div>
+      <br />
+      <div id="word-counter">{getWords()}</div>
+      <br />
+      <div id="char-counter">{getChar()}</div>
     </div>
   );
 };
-
 export default App;
